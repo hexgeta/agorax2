330fd03fa9377eb3b886a833a90e36b8ff31280c
hexgeta <hexgeta@proton.me>
2026-02-18 22:05:12 +0000

    auto-commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Providers } from '@/components/Providers'
 import AppKitProvider from '@/context/AppKitProvider'
 import { Toaster } from '@/components/ui/toaster'
 import { headers } from 'next/headers'
+import PlausibleProvider from 'next-plausible'
 
 // Static layout with revalidation
 export const revalidate = 2592000; // 30 days in seconds
@@ -67,7 +68,7 @@ export default async function RootLayout({
   return (
     <html lang="en" className="font-sans">
       <head>
-       <script async src="https://plausible.io/js/pa-Iid2PZXc5c3t5Iet8tczx.js"></script>
+        <PlausibleProvider domain="agorax.win" />
       </head>
       <body className="min-h-screen bg-black text-white">
         <AppKitProvider cookies={cookies}>
